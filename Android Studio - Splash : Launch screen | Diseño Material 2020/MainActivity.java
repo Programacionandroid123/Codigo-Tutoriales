9b72987e7ce33c01f58a999437f825bd631196e3
codelia-developer <62634579+codelia-developer@users.noreply.github.com>
2020-05-17 22:16:01 -0700

    Nuevo video: Splash Screen!!!
    Agregar archivos de video Splash Screen de Android

---
+++ Android Studio - Splash : Launch screen | Diseño Material 2020/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        // Agregar animaciones
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView deTextView = findViewById(R.id.deTextView);
+        TextView codeliaTextView = findViewById(R.id.codeliaTextView);
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+
+        deTextView.setAnimation(animacion2);
+        codeliaTextView.setAnimation(animacion2);
+        logoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
