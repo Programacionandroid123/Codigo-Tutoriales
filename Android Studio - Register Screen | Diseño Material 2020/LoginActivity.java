7496828f1dc71d5e47494cbb0453543e1289dfbd
codelia-developer <62634579+codelia-developer@users.noreply.github.com>
2020-07-21 17:20:00 -0700

    Login y Register
    Agregar proyectos de login screen y register screen

---
+++ Android Studio - Register Screen | Diseño Material 2020/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.example.login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView bienvenidoLabel, continuarLabel, nuevoUsuario;
+    ImageView loginImageView;
+    TextInputLayout usuarioTextField, contrasenaTextField;
+    MaterialButton inicioSesion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginImageView = findViewById(R.id.loginImageView);
+        bienvenidoLabel = findViewById(R.id.bienvenidoLabel);
+        continuarLabel = findViewById(R.id.continuarLabel);
+        usuarioTextField = findViewById(R.id.usuarioTextField);
+        contrasenaTextField = findViewById(R.id.contrasenaTextField);
+        inicioSesion = findViewById(R.id.inicioSesion);
+        nuevoUsuario = findViewById(R.id.nuevoUsuario);
+
+        nuevoUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+
+                Pair[] pairs = new Pair[7];
+                pairs[0] = new Pair<View, String>(loginImageView, "logoImageTrans");
+                pairs[1] = new Pair<View, String>(bienvenidoLabel, "textTrans");
+                pairs[2] = new Pair<View, String>(continuarLabel, "iniciaSesionTextTrans");
+                pairs[3] = new Pair<View, String>(usuarioTextField, "emailInputTextTrans");
+                pairs[4] = new Pair<View, String>(contrasenaTextField, "passwordInputTextTrans");
+                pairs[5] = new Pair<View, String>(inicioSesion, "buttonSignInTrans");
+                pairs[6] = new Pair<View, String>(nuevoUsuario, "newUserTrans");
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
+                    startActivity(intent, options.toBundle());
+                } else {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+    }
+}
---
+++ Android Studio - Register Screen | Diseño Material 2020/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        // Agregar animaciones
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView deTextView = findViewById(R.id.deTextView);
+        final TextView codeliaTextView = findViewById(R.id.codeliaTextView);
+        final ImageView logoImageView = findViewById(R.id.logoImageView);
+
+        deTextView.setAnimation(animacion2);
+        codeliaTextView.setAnimation(animacion2);
+        logoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(logoImageView, "logoImageTrans");
+                pairs[1] = new Pair<View, String>(codeliaTextView, "textTrans");
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                    startActivity(intent, options.toBundle());
+                } else {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }, 4000);
+    }
+}
